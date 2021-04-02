6bbcb8e458782d260049ac92f161f73be578b7d5
Chris Suran <suran.c@gmail.com>
2021-04-01 20:57:55 -0400

    Add message component and messages service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import { FormsModule } from '@angular/forms';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
-    ItemDetailComponent
+    ItemDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 import { Item } from './item';
 import { ITEMS } from './mock-items';
 //import { Observable, of } from 'rxjs';
@@ -9,8 +10,9 @@ import { ITEMS } from './mock-items';
 })
 export class ItemService {
   getItems(): Observable<Item[]> {
+    this.messageService.add('ItemService: fetched items');
     return of(ITEMS);
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
